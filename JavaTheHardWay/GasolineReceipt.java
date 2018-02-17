01afad759fb5307fd9ae6240fd2775118f9dd791
EliHarper <eli.harper1995@gmail.com>
2018-02-17 16:37:16 -0700

    Added some weirder lines

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,17 @@
+public class GasolineReceipt{
+  public static void main(String[] args){
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |");
+    System.out.println( "|                        |");
+    System.out.println( "|      Corner Store      |");
+    System.out.println( "|                        |");
+    System.out.println( "| 2018-02-17    4:23PM   |");
+    System.out.println( "|                        |");
+    System.out.println( "| Gallons: All of them :(|");
+    System.out.println( "| Price/gallon: 1 soul   |");
+    System.out.println( "|                        |");
+    System.out.println( "|Fuel Total: New climate |");
+    System.out.println( "|                        |");
+    System.out.println( "+------------------------+");
+  }
+}
