cb3a4407091656cb75034e9b198eb42d1e30f7bb
EliHarper <eli.harper1995@gmail.com>
2018-02-18 12:32:56 -0700

    Neat-o. All done with Lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3{
+  public static void main(String[] args){
+    String age = "23";
+    String name = "Eli Harper";
+    String state = "Iowa";
+
+    System.out.println("Name : " + name + "\nAge : " + age + "\nState : " + state);
+  }
+}
